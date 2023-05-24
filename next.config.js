781ca8a4fc541aae51b5318bc6d3669f34573b95
tom-ruiz <tomruiz2611@gmail.com>
2023-05-24 15:25:38 +0200

    next config modif

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 // next.config.js
-const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  assetPrefix: isProd ? '/portfolio-tom/' : '',
-  images: {
-    unoptimized: true,
-  },
-}
+  basePath: process.env.NODE_ENV === 'production' ? '/portfolio-tom-ruiz' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio-tom-ruiz/' : '',
+};
